5e5c1d3da1c3094890e69632402d4542460d8c60
pranavbansal04 <pranavbansal04@gmail.com>
2022-05-24 17:29:15 -0400

    backend config

---
+++ server/config/db.config.js
@@ -0,0 +1,14 @@
+export default {
+    URI: 'mongodb+srv',
+    USERNAME: 'admin',
+    PASSWORD: 'admin123',
+    DB: "HelpOnTheGo",
+    HOST: "cluster0.6udpe.mongodb.net"
+};
+
+// export default {
+//     URI: 'mongodb://',
+//     PORT: 27017,
+//     DB: "HelpOnTheGo",
+//     HOST: "localhost"
+// };
